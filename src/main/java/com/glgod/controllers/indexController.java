a730b9919f1e28e2de0afa019b85aa0e538f3806
Wukai <wukai20101573@163.com>
2016-12-13 00:16:24 +0800

    基础

--- src/main/java/com/glgod/controllers/indexController.java
+++ src/main/java/com/glgod/controllers/indexController.java
@@ -1,7 +1,17 @@
 package com.glgod.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Wukai on 2016/12/8.
  */
+@Controller
 public class indexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index/index";
+    }
 }
---
+++ src/main/webapp/view/index/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Wukai
+  Date: 2016/12/11
+  Time: 14:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+asfsafsa
+</body>
+</html>
